3625bdf48eea76f0c7be258f211286d2a12459cd
Noah Guillory <noah@noahguillory.me>
2022-07-14 20:07:21 -0500

    Initial commit :rocket:

--- provision/terraform/proxmox/k8s-vms.tf
+++ provision/terraform/proxmox/k8s-vms.tf
@@ -1,5 +1,5 @@
-resource "proxmox_vm_qemu" "control-node" {
-  name        = "k8s-0"
+resource "proxmox_vm_qemu" "control-node-1" {
+  name        = "k8s-control-0"
   target_node = "proxmox"
 
   clone = "fedora-36-cloudinit-template"
@@ -8,7 +8,7 @@ resource "proxmox_vm_qemu" "control-node" {
   oncreate = true
   agent    = 1
 
-  memory = 1024
+  memory = 2048
   vcpus  = 1
 
   ipconfig0 = "gw=172.16.0.1,ip=172.16.11.2/16"
@@ -24,8 +24,8 @@ resource "proxmox_vm_qemu" "control-node" {
   }
 }
 
-resource "proxmox_vm_qemu" "worker-node-1" {
-  name        = "k8s-1"
+resource "proxmox_vm_qemu" "control-node-2" {
+  name        = "k8s-control-1"
   target_node = "proxmox"
 
   clone = "fedora-36-cloudinit-template"
@@ -34,7 +34,7 @@ resource "proxmox_vm_qemu" "worker-node-1" {
   oncreate = true
   agent    = 1
 
-  memory = 1024
+  memory = 2048
   vcpus  = 1
 
   ipconfig0 = "gw=172.16.0.1,ip=172.16.11.3/16"
@@ -50,8 +50,8 @@ resource "proxmox_vm_qemu" "worker-node-1" {
   }
 }
 
-resource "proxmox_vm_qemu" "worker-node-2" {
-  name        = "k8s-2"
+resource "proxmox_vm_qemu" "worker-node-1" {
+  name        = "k8s-0"
   target_node = "proxmox"
 
   clone = "fedora-36-cloudinit-template"
@@ -64,6 +64,32 @@ resource "proxmox_vm_qemu" "worker-node-2" {
   vcpus  = 1
 
   ipconfig0 = "gw=172.16.0.1,ip=172.16.11.4/16"
+  network {
+    model  = "e1000"
+    bridge = "vmbr0"
+  }
+
+  disk {
+    type    = "scsi"
+    storage = "main-data"
+    size    = "16G"
+  }
+}
+
+resource "proxmox_vm_qemu" "worker-node-2" {
+  name        = "k8s-1"
+  target_node = "proxmox"
+
+  clone = "fedora-36-cloudinit-template"
+
+  onboot   = true
+  oncreate = true
+  agent    = 1
+
+  memory = 1024
+  vcpus  = 1
+
+  ipconfig0 = "gw=172.16.0.1,ip=172.16.11.5/16"
   network {
     model  = "e1000"
     bridge = "vmbr0"
